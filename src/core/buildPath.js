c2c589a28e75aaf2f3eeebeee52712e3bfef0356
Tauka <k.tauka@gmail.com>
2019-12-22 11:46:28 +0600

    #renderPath support params as query params

--- src/core/buildPath.js
+++ src/core/buildPath.js
@@ -1,44 +1,29 @@
-/**
- * Builds string path from pathTokens and paramsObj
- * @param {Object} routesCfg
- * @param {Array} pathTokens
- * @param {Object} paramsObj
- */
+const renderPath = pathTokens =>
+{
+	return '/' + pathTokens.join('/');
+}
+
+const renderParams = paramsObj =>
+{
+	const searchParams = new URLSearchParams();
+
+	Object.entries(paramsObj).forEach(([ key, val ]) =>
+	{
+		searchParams.set(key, val);
+	});
+
+	const stringParams = searchParams.toString();
+
+	if(!stringParams)
+		return '';
+
+	return `?${stringParams}`;
+}
+
 const buildPath = (routesCfg, pathTokens, paramsObj) =>
 {
-	let currentTokenIdx = 0;
-	let path = '';
-
-	while(currentTokenIdx < pathTokens.length)
-	{
-		const token = pathTokens[currentTokenIdx];
-		path += `/${token}`;
-
-		const { params, optParams } = routesCfg[token];
-
-		if(params && params.length)
-			path += '/' + params
-				.map(param => paramsObj[param])
-				.join('/');
-
-		if(optParams && optParams.length)
-		{
-			const trueOptParams = optParams.reduce((acc, param) =>
-			{
-				if(paramsObj[param])
-					acc.push(paramsObj[param]);
-
-				return acc;
-			}, []);
-
-			if(trueOptParams.length)
-				path += '/' + trueOptParams.join('/');
-		}
-
-		currentTokenIdx ++;
-	}
-
-	return path;
+	return renderPath(pathTokens) +
+		renderParams(paramsObj);
 };
 
 export default buildPath;
