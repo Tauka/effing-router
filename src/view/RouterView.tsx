14299ff16a9e50672c231d0fc8ef01eca8c8e6ea
Tauyekel Kunzhol <k.tauka@gmail.com>
2020-06-19 19:30:03 +0600

    view: fix symbol ts

--- src/view/RouterView.tsx
+++ src/view/RouterView.tsx
@@ -8,7 +8,7 @@ interface BuildComponentProps {
 	routesCfg: RoutesConfiguration;
 	currentTokenIdx: number;
 	extraProps?: ExtraProps;
-	routes: string[];
+	routes: (string | symbol)[];
 }
 
 interface RouterViewProps {
@@ -24,24 +24,24 @@ const BuildComponent: React.FC<BuildComponentProps> = ({ routesCfg, currentToken
 
 	const childRoute = (props: ExtraProps) =>
 	{
-		if(!routesCfg[token].children)
+		if(!routesCfg[token as any].children)
 			return null;
 
 		return <BuildComponent
 			extraProps={props}
-			routesCfg={routesCfg[token].children as Record<string, RouteObject>}
+			routesCfg={routesCfg[token as any].children as Record<string, RouteObject>}
 			currentTokenIdx={currentTokenIdx + 1}
 			routes={routes}
 		/>;
 	}
 
-	if(!routesCfg[token])
-		throw new Error(`Cannot find route ${token} in path [${routes}]`);
+	if(!routesCfg[token as any])
+		throw new Error(`Cannot find route ${String(token)} in path [${routes}]`);
 
-	const Component = routesCfg[token].component;
+	const Component = routesCfg[String(token)].component;
 
 	if(!Component)
-		throw new Error(`Cannot find component in "${token}"`);
+		throw new Error(`Cannot find component in "${String(token)}"`);
 
 	return <Component {...extraProps} childRoute={childRoute}/>
 };
